df0a4def2c94882ffbd202bae4ef2923b84f6ab2
Anmizi <1845513904@qq.com>
2021-09-12 23:36:46 +0800

    首页头部和图片换肤功能

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
-  <div id="app">
-    <div></div>
-  </div>
+  <Header />
 </template>
-
+<script>
+import Header from './components/Header.vue'
+export default {
+  name: 'App',
+  components: { Header }
+}
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-div{
-  width: 100px;
-  height: 100px;
-}
 </style>
