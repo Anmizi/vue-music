c8f0f1c86a2203ca4022ccc78d9b52594884c62c
Anmizi <1845513904@qq.com>
2021-09-16 12:21:38 +0800

    首页推荐歌单组件

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 // 专门用于管理处理各种请求接口
 import Network from './network'
 
-export const getBanner = () => Network.get('/banner?type=2')
+export const getBanner = () => Network.get('banner?type=2')
+export const getPersonalized = () => Network.get('personalized')
--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="recommend">
     <Banner :banners="banners"></Banner>
+   <Personalized :personalized="personalized"></Personalized>
   </div>
 </template>
 
 <script>
-import { getBanner } from '../api/index'
+import { getBanner, getPersonalized } from '../api/index'
 import Banner from '../components/Banner'
+import Personalized from '../components/Personalized'
 export default {
   name: 'Recommend',
-  components: { Banner },
+  components: { Banner, Personalized },
   data () {
     return {
-      banners: []
+      banners: [],
+      personalized: []
     }
   },
   created () {
     getBanner()
       .then(data => {
         this.banners = data.banners
+      }).catch(error => {
+        console.log(error)
+      })
+    getPersonalized()
+      .then(data => {
+        this.personalized = data.result
       }).catch(error => {
         console.log(error)
       })
