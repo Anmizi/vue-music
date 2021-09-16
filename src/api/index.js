f93bd16d9036d6a90e72f3fc0386b69d44b19cc8
Anmizi <1845513904@qq.com>
2021-09-16 21:20:45 +0800

    添加最新专辑接口

--- src/api/index.js
+++ src/api/index.js
@@ -2,4 +2,5 @@
 import Network from './network'
 
 export const getBanner = () => Network.get('banner?type=2')
-export const getPersonalized = () => Network.get('personalized')
+export const getPersonalized = () => Network.get('personalized?limit=6')
+export const getAlbum = () => Network.get('album/newest')
