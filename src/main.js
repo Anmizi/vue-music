7e70f577682a1704ed117ab4eff12b421e9ac7bb
Anmizi <1845513904@qq.com>
2021-09-18 22:21:34 +0800

    添加图片懒加载

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueLazyload from 'vue-lazyload'
 import fastclick from 'fastclick'
 import './assets/css/base.scss'
+
+Vue.use(VueLazyload, {
+  loading: require('./assets/images/loading1.gif')
+})
 fastclick.attach(document.body)
 Vue.config.productionTip = false
 
