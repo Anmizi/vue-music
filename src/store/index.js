b817d011e534468a451a81401803be5301722ea5
Anmizi <1845513904@qq.com>
2021-09-23 21:58:23 +0800

    默认播放界面显示隐藏

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,26 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // state保存全局共享数据
   state: {
+    isFullScreen: false
   },
+  // 用于保存修改全局共享数据的方法
   mutations: {
+    changeFullScreen (state, flag) {
+      state.isFullScreen = flag
+    }
   },
+  // actions用于保存触发mutations中的方法
   actions: {
+    setFullScreen ({ commit }, flag) {
+      commit('changeFullScreen', flag)
+    }
+  },
+  getters: {
+    isFullScreen (state) {
+      return state.isFullScreen
+    }
   },
   modules: {
   }
