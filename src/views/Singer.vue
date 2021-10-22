1b60ab2bf1bf9599007961e57e972c9b93f3fdf5
Anmizi <1845513904@qq.com>
2021-10-22 20:43:17 +0800

    :beers: 歌手界面歌手数据填充

--- src/views/Singer.vue
+++ src/views/Singer.vue
@@ -1,26 +1,87 @@
 <template>
   <div class="singer">
-    歌手
+    <ScrollView>
+      <ul class="list-wrapper">
+        <li class="list-group" v-for="(value,index) in list" :key="index">
+          <h2 class="group-title">{{keys[index]}}</h2>
+          <ul>
+            <li class="group-item" v-for="obj in list[index]" :key="obj.id">
+              <img v-lazy="obj.img1v1Url" alt="">
+              <p>{{obj.name}}</p>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </ScrollView>
   </div>
 </template>
 
 <script>
-// import { getLetterArtists } from '../api/index'
 import { getAllArtists } from '../api/index'
+import ScrollView from '../components/ScrollView.vue'
 export default {
   name: 'Singer',
+  components: {
+    ScrollView
+  },
   created () {
     getAllArtists()
       .then(result => {
-        console.log(result)
+        this.keys = result.keys
+        this.list = result.list
       })
       .catch(error => {
         console.log(error)
       })
+  },
+  data () {
+    return {
+      keys: [],
+      list: []
+    }
   }
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+@import '../assets/css/mixin';
+.singer{
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 184px;
+  bottom: 0;
+  @include bg_sub_color();
+  overflow: hidden;
+  .list-wrapper{
+    .list-group{
+      .group-title{
+        @include bg_color();
+        @include font_size($font_medium);
+        padding: 10px 20px;
+        box-sizing: border-box;
+        color: #fff;
+      }
+      .group-item{
+        display: flex;
+        padding: 10px 20px;
+        justify-content: flex-start;
+        border-bottom: 1px solid #ccc;
+        img{
+          width: 100px;
+          height: 100px;
+          border-radius: 50%;
+          overflow: hidden;
+        }
+        p{
+          display: flex;
+          align-items: center;
+          margin-left: 20px;
+          @include font_size($font_medium);
+          @include font_color();
+        }
+      }
+    }
+  }
+}
 </style>
