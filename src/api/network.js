8840a9935d4eb987baf5766a741f1259aaac2755
Anmizi <1845513904@qq.com>
2021-10-27 16:36:26 +0800

    :beers: 优化网络提示

--- src/api/network.js
+++ src/api/network.js
@@ -1,8 +1,34 @@
 import axios from 'axios'
+import Vue from 'vue'
 
 // 进行全局配置
 axios.defaults.baseURL = 'http://10.77.169.66:8080'
 axios.defaults.timeout = 3000
+// 添加请求拦截器
+let count = 0
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  count++
+  Vue.showLoading()
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  count--
+  if (count === 0) {
+    Vue.hiddenLoading()
+  }
+  return response
+}, function (error) {
+  // 对响应错误做点什么
+  Vue.hiddenLoading()
+  return Promise.reject(error)
+})
 
 // 封装get/post方法
 
--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueLazyload from 'vue-lazyload'
+import Loading from './plugin/Loading/index'
 import fastclick from 'fastclick'
 import './assets/css/base.scss'
 // 图片懒加载
 Vue.use(VueLazyload, {
   loading: require('./assets/images/loading1.gif')
 })
+// 使用Loading插件
+Vue.use(Loading)
 fastclick.attach(document.body)
 Vue.config.productionTip = false
 
